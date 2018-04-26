11b1852028e037f2ed9d36087f4285fa8555d84a
JavaTMP <support@javatmp.com>
2018-04-26 13:02:29 +0400

    #59 & #83 create document class to holp uploaded file

---
+++ JavaTMP-Java-Ajax-Starter/src/java/com/javatmp/domain/Document.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javatmp.domain;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Document {
+
+    public Long documentId;
+    public String documentName;
+    public long documentSize;
+    public String contentType;
+    public byte[] documentContent;
+
+}
