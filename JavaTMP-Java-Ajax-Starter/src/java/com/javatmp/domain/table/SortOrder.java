b9321846b68ca9096d93ca29390a63500989f97b
JavaTMP <support@javatmp.com>
2018-05-05 16:26:53 +0400

    Datatables with AJAX contents and paginations #65

---
+++ JavaTMP-Java-Ajax-Starter/src/java/com/javatmp/domain/table/SortOrder.java
@@ -0,0 +1,52 @@
+package com.javatmp.domain.table;
+
+public enum SortOrder {
+
+    /**
+     * The asc.
+     */
+    ASC("ASC"),
+    /**
+     * The desc.
+     */
+    DESC("DESC");
+
+    /**
+     * The value.
+     */
+    private final String value;
+
+    /**
+     * Instantiates a new sort order.
+     *
+     * @param v the v
+     */
+    SortOrder(String v) {
+        value = v;
+    }
+
+    /**
+     * From value.
+     *
+     * @param v the v
+     * @return the sort order
+     */
+    public static SortOrder fromValue(String v) {
+        for (SortOrder c : SortOrder.values()) {
+            if (c.name().equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+    /**
+     * Value.
+     *
+     * @return the string
+     */
+    public String value() {
+        return value;
+    }
+
+}
