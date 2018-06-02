7048e27314f13ffae8dd6cbfca83cbf50deac457
JavaTMP <support@javatmp.com>
2018-06-03 00:05:23 +0400

    cleaning and formatting classes

--- JavaTMP-Java-Ajax-Starter/src/java/com/javatmp/web/controller/LoginController.java
+++ JavaTMP-Java-Ajax-Starter/src/java/com/javatmp/web/controller/LoginController.java
@@ -9,7 +9,6 @@ import com.javatmp.util.MD5Util;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,16 +25,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        ServicesFactory sf = (ServicesFactory) request.getSession().getAttribute(Constants.SERVICES_FACTORY_ATTRIBUTE_NAME);
-        ResourceBundle bundle = (ResourceBundle) request.getSession().getAttribute(Constants.LANGUAGE_ATTR_KEY);
-        logger.info("user bundle [" + bundle + "]");
-        logger.info("user locale [" + bundle.getLocale() + "]");
-        logger.info("user locale lang [" + bundle.getLocale().getLanguage() + "]");
-        logger.info("user locale country [" + bundle.getLocale().getCountry() + "]");
-        // Create a fake user
         request.getRequestDispatcher("/WEB-INF/pages/custom-pages/login-pages/default-login-page.jsp").forward(request, response);
-
     }
 
     @Override
@@ -50,7 +40,6 @@ public class LoginController extends HttpServlet {
         try {
             MvcHelper.populateBeanByRequestParameters(request, user);
             logger.info("Check User [" + MvcHelper.deepToString(user) + "]");
-            logger.info("sf [" + sf + "]");
             User dbUser = sf.getUserService().readUserByUsername(user);
 
             if (dbUser != null && dbUser.getPassword().equals(MD5Util.convertToMD5(user.getPassword()))) {
