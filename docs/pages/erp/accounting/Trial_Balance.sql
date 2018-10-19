8955a22c44ce772e202ec75466e231cb7499b016
JavaTMP <support@javatmp.com>
2018-10-19 22:27:47 +0400

    implementing accounting features and sql

--- docs/pages/erp/accounting/Trial_Balance.sql
+++ docs/pages/erp/accounting/Trial_Balance.sql
@@ -9,21 +9,25 @@
  */
 -- 1) Using SQL to produce Trial Balance
 select Journal_T.GL_ID as 'Acct Code',GL_Name_VC as 'Acct Name',
- case when sum(Amount_NU) > 0 then sum(Amount_NU) else 0 end as 'Debit Bal',
- case when sum(Amount_NU) < 0 then (sum(Amount_NU) * -1) else 0 end as 'Credit Bal'
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then Amount_NU
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1) else 0 end end) as 'Debit Bal',
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1)
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then (Amount_NU) else 0 end end) as 'Credit Bal'
 from Journal_T join COA_T
 on Journal_T.GL_ID = COA_T.GL_ID
-where BS_Category_VC in ('FA_Cost', 'FA', 'CA', 'CL', 'SF', 'Rev', 'Cos', 'Otc', 'Oic', 'Tax')
+-- where BS_Category_VC in ('FA_Cost', 'FA', 'CA', 'CL', 'SF', 'Rev', 'Cos', 'Otc', 'Oic', 'Tax')
 group by GL_Name_VC,Journal_T.GL_ID
 
 union all
-select '','Total',
- sum(case when Amount_NU > 0 then Amount_NU else 0 end) as 'Debit Bal',
- sum(case when Amount_NU < 0 then (Amount_NU * -1) else 0 end) as 'Credit Bal'
+select '','',
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then Amount_NU
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1) else 0 end end) as 'Debit Bal',
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1)
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then (Amount_NU) else 0 end end) as 'Credit Bal'
 from Journal_T join COA_T
 on Journal_T.GL_ID = COA_T.GL_ID
-where
-BS_Category_VC in ('FA_Cost', 'FA', 'CA', 'CL', 'SF', 'Rev', 'Cos', 'Otc', 'Oic', 'Tax')
+-- where
+-- BS_Category_VC in ('FA_Cost', 'FA', 'CA', 'CL', 'SF', 'Rev', 'Cos', 'Otc', 'Oic', 'Tax')
 
 union all
 select '','Total BS Movement in YR 2007',
--- docs/pages/erp/accounting/Trial_Balance_New.sql
+++ docs/pages/erp/accounting/Trial_Balance_New.sql
@@ -1,14 +1,14 @@
-select accountId, acctName, acctCat, debits, credits,(
-case when acctCat in ('FA_Cost','FA','CA','Cos','Otc','Tax') then (debits - credits) else (credits - debits) end
-) as balance
+select accountId, acctName, acctCat, debits, credits,(debits - credits) as balance
 from (
 select Journal_T.GL_ID as accountId,GL_Name_VC as acctName, BS_Category_VC as acctCat,
- sum(case when Amount_NU > 0 then Amount_NU else 0 end) as debits,
- sum(case when Amount_NU < 0 then (Amount_NU * -1) else 0 end) as credits
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then Amount_NU
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1) else 0 end end) as debits,
+ sum(case when BS_Category_VC in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU < 0 then (Amount_NU * -1)
+else case when BS_Category_VC not in ('FA_Cost','FA','CA','Cos','Otc','Tax') and Amount_NU > 0 then (Amount_NU) else 0 end end) as credits
 from Journal_T join COA_T
 on Journal_T.GL_ID = COA_T.GL_ID
 --where BS_Category_VC in ('FA_Cost', 'FA', 'CA', 'CL', 'SF', 'Rev', 'Cos', 'Otc', 'Oic', 'Tax')
-group by GL_Name_VC,Journal_T.GL_ID) acctList ;
+group by Journal_T.GL_ID, GL_Name_VC, BS_Category_VC) acctList;
 
 
 -- cat increase on debit : 'FA_Cost','FA','CA','Cos','Otc','Tax'
