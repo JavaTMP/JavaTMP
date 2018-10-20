e529895bc91cf4dda3db21cee6fc389cf794ca48
JavaTMP <support@javatmp.com>
2018-10-21 00:01:40 +0400

    implementing accounting features and sql

--- docs/pages/erp/accounting/create.sql
+++ docs/pages/erp/accounting/create.sql
@@ -10,11 +10,12 @@
 DROP TABLE IF EXISTS account;
 DROP TABLE IF EXISTS accountGroup;
 DROP TABLE IF EXISTS accountType;
+DROP TABLE IF EXISTS accountTransaction;
 DROP TABLE IF EXISTS transaction;
 DROP TABLE IF EXISTS transactionType;
 
 CREATE TABLE accountType (
-    id int(1) not null,
+    id int(1) UNSIGNED not null,
     name varchar(32) not null,
     debitSign int(1) not null,
     creditSign int(1) not null,
@@ -29,26 +30,26 @@ INSERT INTO accountType (id, name, debitSign, creditSign) VALUES
 (5, 'Expense account', +1, -1);
 
 CREATE TABLE accountGroup (
-    id int(3) not null,
+    id int(3) UNSIGNED not null,
     name varchar(32) not null,
     description varchar(1024),
-    accountType int(1),
+    accountType int(1) UNSIGNED,
     CONSTRAINT accountGroup_id_pk PRIMARY KEY (id),
     CONSTRAINT accountGroup_accountType_fk FOREIGN KEY (accountType) REFERENCES accountType (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE account (
-    id int UNSIGNED not null AUTO_INCREMENT,
+    id BIGINT UNSIGNED not null AUTO_INCREMENT,
     accountCode varchar(32) not null,
     name varchar(32) not null,
     description varchar(1024),
-    accountGroup int(3),
+    accountGroup int(3) UNSIGNED,
     debit DECIMAL(32,8),
     credit DECIMAL(32,8),
     balance DECIMAL(33,8),
     status TINYINT,
     creationDate TIMESTAMP NOT NULL,
-    parentAccount int UNSIGNED,
+    parentAccount BIGINT UNSIGNED,
     CONSTRAINT account_id_pk PRIMARY KEY (id),
     CONSTRAINT account_accountGroup_fk FOREIGN KEY (accountGroup) REFERENCES accountGroup (id),
     CONSTRAINT account_parentAccount_fk FOREIGN KEY (parentAccount) REFERENCES account (id)
@@ -64,14 +65,25 @@ CREATE TABLE transactionType (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE transaction (
-    id BIGINT not null AUTO_INCREMENT,
+    id BIGINT UNSIGNED not null AUTO_INCREMENT,
     referenceCode varchar(1024),
     transactionType int not null,
     transactionDate DATETIME not null,
     description varchar(1024),
-
     status TINYINT,
     creationDate TIMESTAMP NOT NULL,
     CONSTRAINT transaction_id_pk PRIMARY KEY (id),
     CONSTRAINT transaction_transactionType_fk FOREIGN KEY (transactionType) REFERENCES transactionType (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE accountTransaction (
+    id BIGINT UNSIGNED not null AUTO_INCREMENT,
+    transactionId BIGINT UNSIGNED not null,
+    accountId BIGINT UNSIGNED not null,
+    status TINYINT,
+    creationDate TIMESTAMP NOT NULL,
+    CONSTRAINT accountTransaction_id_pk PRIMARY KEY (id),
+    CONSTRAINT accountTransaction_tranId_fk FOREIGN KEY (transactionId) REFERENCES transaction (id),
+    CONSTRAINT accountTransaction_acctId_fk FOREIGN KEY (accountId) REFERENCES account (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
