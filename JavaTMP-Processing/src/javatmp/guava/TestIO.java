16f843c23361cb22d03df19435c1fc8cdc34e6f6
JavaTMP <support@javatmp.com>
2019-05-16 14:06:26 +0400

    #206 add guava

---
+++ JavaTMP-Processing/src/javatmp/guava/TestIO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatmp.guava;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class TestIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        byte[] data = Files.toByteArray(new File("C:\\Users\\Mohamed\\output.txt"));
+        System.out.println("file lenght [" + data.length + "]");
+
+    }
+
+}
