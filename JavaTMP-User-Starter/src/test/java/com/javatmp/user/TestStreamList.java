6376df957eaf859e0b71e05e83eb2ddd57188ea5
JavaTMP <support@javatmp.com>
2019-11-24 03:01:58 +0400

    doc Generic repository using the JPA Criteria API #256 and remove JpaDaoHelper class and refactor all application

---
+++ JavaTMP-User-Starter/src/test/java/com/javatmp/user/TestStreamList.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javatmp.user;
+
+import com.javatmp.module.user.entity.User;
+import com.javatmp.module.user.service.UserService;
+import java.util.Optional;
+import java.util.stream.Stream;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import org.hibernate.jpa.QueryHints;
+
+/**
+ *
+ * @author JavaTMP
+ */
+public class TestStreamList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UserService userService;
+        userService = new UserService(Persistence.createEntityManagerFactory("AppPU"));
+        EntityManager em = null;
+        try {
+            em = userService.getEntityManagerFactory().createEntityManager();
+            em.getTransaction().begin();
+            Stream<User> retList = em.createNativeQuery(
+                    "select * from user p "
+                    + "for update skip locked", User.class)
+                    .setHint(QueryHints.HINT_FETCH_SIZE, 1)
+                    .getResultStream();
+            Optional<User> u = retList.findFirst();
+            User user = u.get();
+
+            System.out.println("user [" + user.getId() + "]");
+            user.setAddress("here");
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            Throwable t = e;
+            while (t != null) {
+                System.out.println("type [" + e.getClass().getName() + "]");
+                System.out.println("e [" + e.getMessage() + "]");
+                t = t.getCause();
+            }
+        }
+
+    }
+
+}
