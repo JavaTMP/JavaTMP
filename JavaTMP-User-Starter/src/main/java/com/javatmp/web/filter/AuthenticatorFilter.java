15017897593931312fcef6f5aac7edf8b75fa98a
JavaTMP <support@javatmp.com>
2019-11-30 17:21:11 +0400

    support fake user123 account for any new session for demo

--- JavaTMP-User-Starter/src/main/java/com/javatmp/web/filter/AuthenticatorFilter.java
+++ JavaTMP-User-Starter/src/main/java/com/javatmp/web/filter/AuthenticatorFilter.java
@@ -1,14 +1,16 @@
 package com.javatmp.web.filter;
 
-import com.javatmp.module.user.entity.User;
-import com.javatmp.fw.mvc.MvcHelper;
 import com.javatmp.fw.domain.ResponseMessage;
+import com.javatmp.fw.mvc.MvcHelper;
+import com.javatmp.module.user.entity.User;
 import com.javatmp.util.Constants;
+import com.javatmp.util.ServicesFactory;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -29,6 +31,8 @@ public class AuthenticatorFilter extends FilterWrapper {
         // https://stackoverflow.com/questions/46592664/request-getservletpath-returned-null-from-spring-mvc
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
+        ServletContext context = request.getServletContext();
+        ServicesFactory sf = (ServicesFactory) context.getAttribute(Constants.SERVICES_FACTORY_ATTRIBUTE_NAME);
         String path = req.getRequestURI().substring(req.getContextPath().length());
         HttpSession session = req.getSession();
         ResourceBundle labels = (ResourceBundle) session.getAttribute(Constants.LANGUAGE_ATTR_KEY);
@@ -43,6 +47,17 @@ public class AuthenticatorFilter extends FilterWrapper {
             User user = (User) session.getAttribute("user");
             if (user != null) {
                 logger.info("Session User is [" + MvcHelper.deepToString(user) + "]");
+                chain.doFilter(request, response);
+            } else if (session.isNew()) {
+                // for demo and for new time access we support user123 loggedin:
+                User dbUser = new User();
+                dbUser.setUserName("user123");
+                dbUser = sf.getUserService().readUserByUsername(dbUser);
+                Locale locale = Locale.forLanguageTag(dbUser.getLang());
+                ResourceBundle bundle = ResourceBundle.getBundle(Constants.RESOURCE_BUNDLE_BASE_NAME, locale);
+                session.setAttribute(Constants.LANGUAGE_ATTR_KEY, bundle);
+                session.setAttribute("user", dbUser);
+                sf.getUserService().updateLastUserAccess(dbUser);
                 chain.doFilter(request, response);
             } else if ("ajax".equals(req.getParameter("_ajax")) || req.getMethod().equals("POST")) {
                 // we send an error ajax message response consisting
