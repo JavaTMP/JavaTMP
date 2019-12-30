1f07411af522940f02d82a8be55c4ad23262e4e8
JavaTMP <Mohamed@Mohamed-PC>
2019-12-30 10:29:16 +0400

    #279 #259 use start.spring.io to create initial spring boot application

---
+++ JavaTMP-App-Starter/src/main/java/com/javatmp/JavaTmpAppStarterApplication.java
@@ -0,0 +1,13 @@
+package com.javatmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTmpAppStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTmpAppStarterApplication.class, args);
+	}
+
+}
---
+++ JavaTMP-App-Starter/src/test/java/com/javatmp/JavaTmpAppStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTmpAppStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
