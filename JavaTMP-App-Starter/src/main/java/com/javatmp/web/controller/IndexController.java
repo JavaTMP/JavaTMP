4558b1df87b7a7eb56c98e6519dfe1a16b44e7c4
JavaTMP <support@javatmp.com>
2019-12-30 22:47:37 +0400

    #279 #259 upgrade index and pages controllers

--- JavaTMP-App-Starter/src/main/java/com/javatmp/web/controller/IndexController.java
+++ JavaTMP-App-Starter/src/main/java/com/javatmp/web/controller/IndexController.java
@@ -1,14 +1,34 @@
 package com.javatmp.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping("")
+//    @ResponseBody
     public String index() {
+        log.debug("index page is being calling");
         return "/WEB-INF/index.jsp";
     }
+
+    @RequestMapping("/pages/**")
+    public String pages(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        System.out.println("request path is [" + requestURI + "]");
+        log.debug("request path is [" + requestURI + "]");
+        String requestPage = null;
+        if (!requestURI.endsWith(".html")) {
+            requestPage = "/WEB-INF" + requestURI + ".jsp";
+        } else {
+            requestPage = "/WEB-INF" + requestURI;
+        }
+        log.info("Request Page [" + requestPage + "]");
+        return requestPage;
+    }
+
 }
