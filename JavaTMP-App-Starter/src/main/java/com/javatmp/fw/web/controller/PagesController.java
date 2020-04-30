a8626863f4158ff9c8ebe443c7b2f9a9ad0a1c71
JavaTMP <support@javatmp.com>
2020-05-01 03:44:42 +0400

    Solve the bug when deploying using context root other than / or when deploying to standalone tomcat server. by trimming the context path in PagesController.java.

--- JavaTMP-App-Starter/src/main/java/com/javatmp/fw/web/controller/PagesController.java
+++ JavaTMP-App-Starter/src/main/java/com/javatmp/fw/web/controller/PagesController.java
@@ -1,24 +1,21 @@
 package com.javatmp.fw.web.controller;
 
-import com.javatmp.module.user.entity.Countrytranslation;
-import com.javatmp.module.user.service.CountryService;
-import com.javatmp.module.user.entity.Languagetranslation;
-import com.javatmp.module.user.service.LanguageService;
-import com.javatmp.module.user.entity.Themetranslation;
-import com.javatmp.module.user.service.ThemeService;
-import com.javatmp.module.user.entity.Timezonetranslation;
-import com.javatmp.module.user.service.TimezoneService;
-import com.javatmp.module.user.entity.User;
 import com.javatmp.fw.util.Constants;
-import java.util.List;
-import java.util.Optional;
-import java.util.ResourceBundle;
-import javax.servlet.http.HttpServletRequest;
+import com.javatmp.module.user.entity.*;
+import com.javatmp.module.user.service.CountryService;
+import com.javatmp.module.user.service.LanguageService;
+import com.javatmp.module.user.service.ThemeService;
+import com.javatmp.module.user.service.TimezoneService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 @Slf4j
 @Controller
@@ -37,9 +34,14 @@ public class PagesController {
     protected String doGet(@SessionAttribute ResourceBundle labels, @SessionAttribute(Constants.LOG_IN_USER_NAME) Optional<User> loggedInUser,
             HttpServletRequest request) {
 
+        String contextPath = request.getContextPath();
         String requestUrl = request.getRequestURI();
+        // Solve problem for context path not "/" : remove context path from request url:
+        if(contextPath != null && contextPath.length() > 0) {
+            requestUrl = requestUrl.substring(contextPath.length());
+        }
         String requestPage = requestUrl + ".jsp";
-        log.info("request url [" + requestUrl + "] Request Page [" + requestPage + "]");
+        log.info("contextPath [" + contextPath + "], request url [" + requestUrl + "] Request Page [" + requestPage + "]");
         User user = new User(0L, labels.getString("global.language"));
         if (loggedInUser.isPresent()) {
             user = loggedInUser.get();
