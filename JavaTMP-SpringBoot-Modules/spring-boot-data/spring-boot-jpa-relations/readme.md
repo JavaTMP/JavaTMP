4b6a7c07790aa35099b46ba58e64ffae01e07d3e
JavaTMP <support@javatmp.com>
2020-12-12 15:06:38 +0400

    add

--- JavaTMP-SpringBoot-Modules/spring-boot-data/spring-boot-jpa-relations/readme.md
+++ JavaTMP-SpringBoot-Modules/spring-boot-data/spring-boot-jpa-relations/readme.md
@@ -1,1 +1,3 @@
 # Spring Boot JPA Relations Demo Module
+- [https://www.callicoder.com/hibernate-spring-boot-jpa-one-to-one-mapping-example/](https://www.callicoder.com/hibernate-spring-boot-jpa-one-to-one-mapping-example/)
+- 
--- JavaTMP-SpringBoot-Modules/spring-boot-data/spring-boot-jpa-relations/src/main/java/com/javatmp/demo/jpa/JpaDemoApplication.java
+++ JavaTMP-SpringBoot-Modules/spring-boot-data/spring-boot-jpa-relations/src/main/java/com/javatmp/demo/jpa/JpaDemoApplication.java
@@ -1,7 +1,6 @@
 package com.javatmp.demo.jpa;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,43 +13,8 @@ public class JpaDemoApplication implements CommandLineRunner {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
 
-	@Autowired
-	CustomerRepository repository;
-
 	@Override
 	public void run(String... strings) throws Exception {
-
-		// save a few customers
-		repository.save(new Customer(null,"Jack", "Bauer"));
-		repository.save(new Customer(null,"Chloe", "O'Brian"));
-		repository.save(new Customer(null,"Kim", "Bauer"));
-		repository.save(new Customer(null,"David", "Palmer"));
-		repository.save(new Customer(null,"Michelle", "Dessler"));
-
-		// fetch all customers
-		log.info("Customers found with findAll():");
-		log.info("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			log.info(customer.toString());
-		}
-		log.info("");
-
-		// fetch an individual customer by ID
-		Customer customer = repository.findById(1L);
-		log.info("Customer found with findById(1L):");
-		log.info("--------------------------------");
-		log.info(customer.toString());
-		log.info("");
-
-		// fetch customers by last name
-		log.info("Customer found with findByLastName('Bauer'):");
-		log.info("--------------------------------------------");
-		repository.findByLastName("Bauer").forEach(bauer -> {
-			log.info("customer : {}", bauer.toString());
-		});
-		// for (Customer bauer : repository.findByLastName("Bauer")) {
-		//  log.info(bauer.toString());
-		// }
-		log.info("**************");
+		log.debug("welcome");
 	}
 }
