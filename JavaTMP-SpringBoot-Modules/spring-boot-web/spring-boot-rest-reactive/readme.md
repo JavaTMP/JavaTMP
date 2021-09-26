3ee2ac128915be6ad7492946cd3f7b5f3fe5e294
JavaTMP <support@javatmp.com>
2021-09-26 23:53:38 +0400

    add webflux and reactive module for rest api

--- JavaTMP-SpringBoot-Modules/spring-boot-web/spring-boot-rest-reactive/readme.md
+++ JavaTMP-SpringBoot-Modules/spring-boot-web/spring-boot-rest-reactive/readme.md
@@ -6,3 +6,5 @@
 - [https://howtodoinjava.com/spring-webflux/reactive-websockets/](https://howtodoinjava.com/spring-webflux/reactive-websockets/)
 - [https://mkyong.com/spring-boot/spring-boot-webflux-server-sent-events-example/](https://mkyong.com/spring-boot/spring-boot-webflux-server-sent-events-example/)
 - [https://mkyong.com/spring-boot/spring-boot-webflux-thymeleaf-reactive-example/](https://mkyong.com/spring-boot/spring-boot-webflux-thymeleaf-reactive-example/)
+
+### Flux References
---
+++ JavaTMP-SpringBoot-Modules/spring-boot-web/spring-boot-rest-reactive/src/main/java/com/javatmp/demo/rest/reactive/FluxRestAPIController.java
@@ -0,0 +1,69 @@
+package com.javatmp.demo.rest.reactive;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/rest/flux")
+@Slf4j
+public class FluxRestAPIController {
+
+    @Autowired
+    private Set<RestDto> restDtoRepository;
+
+    @Autowired
+    private Scheduler reactiveScheduler;
+
+    @GetMapping("/get")
+    Flux<RestDto> all() {
+        log.debug("requested get request");
+        return Flux.fromStream(()-> this.restDtoRepository.stream())
+                .subscribeOn(reactiveScheduler);
+    }
+
+    @GetMapping("/get/{id}")
+    Mono<RestDto> get(@PathVariable Long id) {
+        log.info("try getting id :{}", id);
+        return Mono.fromCallable(() -> {
+            return restDtoRepository.stream()
+                    .filter(restDto -> restDto.getId().equals(id))
+                    .findAny()
+                    .orElseThrow(() ->
+                            new IllegalArgumentException(
+                                    String.format("id not found : %d .", id)));
+        }).subscribeOn(reactiveScheduler);
+    }
+
+    @PostMapping("/post")
+    Mono<RestDto> post(@RequestBody RestDto restDto) {
+        return Mono.fromCallable(() -> {
+            this.restDtoRepository.add(restDto);
+            return restDto;
+        }).subscribeOn(reactiveScheduler);
+    }
+
+    @PutMapping("/put/{id}")
+    Mono<RestDto> put(@RequestBody RestDto restDto, @PathVariable Long id) {
+        return Mono.fromCallable(() -> {
+            restDto.setId(id);
+            this.restDtoRepository.add(restDto);
+            return restDto;
+        }).subscribeOn(reactiveScheduler);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    Mono<Void> delete(@PathVariable Long id) {
+        return Mono.fromRunnable(() ->
+                this.restDtoRepository.remove(
+                        RestDto.builder().id(id).build()
+                )
+        ).subscribeOn(reactiveScheduler).then();
+    }
+}
