c2dc4df67eab53d713d5e90db9e812c38b4805af
JavaTMP <support@javatmp.com>
2021-10-18 14:30:25 +0400

    add open api or swagger

---
+++ JavaTMP-SpringBoot-Modules/spring-boot-miscellaneous/spring-boot-OpenAPI/src/test/java/com/javatmp/demo/rest/OnlyRestAPIControlerMockMvcTests.java
@@ -0,0 +1,26 @@
+package com.javatmp.demo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+@AutoConfigureMockMvc
+@WebMvcTest(RestAPIController.class)
+public class OnlyRestAPIControlerMockMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/rest/get").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
