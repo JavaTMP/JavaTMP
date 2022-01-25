609c80a908247eeab1508473dc219bbe446512c6
JavaTMP <support@javatmp.com>
2022-01-25 22:01:53 +0400

    refactor files

--- JavaTMP-SpringBoot-Modules/spring-boot-threading/spring-boot-thread-task-scheduler/src/main/java/com/javatmp/demo/TaskSchedulerApplication.java
+++ JavaTMP-SpringBoot-Modules/spring-boot-threading/spring-boot-thread-task-scheduler/src/main/java/com/javatmp/demo/TaskSchedulerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
@@ -35,15 +36,20 @@ public class TaskSchedulerApplication {
             log.info("*** Start Spring Boot Project ***");
             IntStream.range(1, 10).forEach(s -> {
                 PrintTask thread = applicationContext.getBean(PrintTask.class);
-                thread.setName("thread " + s);
+                thread.setName("thread-execute-" + s);
                 threadPoolTaskScheduler.execute(thread);
             });
 
-            IntStream.range(11, 12).forEach(s -> {
+            int threadCount = 12;
+            IntStream.range(1, threadCount).forEach(s -> {
                 PrintTask thread = applicationContext.getBean(PrintTask.class);
-                thread.setName("thread " + s);
+                thread.setName("thread-schedule-" + s);
+                long initialDelay = 15000;
+                long actualDelay = initialDelay + ((initialDelay/threadCount) * s);
+                log.info("actual delay of thread :{} is :{}", s, actualDelay);
                 int randomDelay = ThreadLocalRandom.current().nextInt(1000, 2000);
-                threadPoolTaskScheduler.scheduleWithFixedDelay(thread, randomDelay);
+                threadPoolTaskScheduler.scheduleWithFixedDelay(
+                        thread, Duration.ofMillis(actualDelay));
             });
 
 //            for (;;) {
