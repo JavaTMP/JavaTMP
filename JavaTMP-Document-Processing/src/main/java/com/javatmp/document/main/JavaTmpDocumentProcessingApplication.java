4ac7d2cb146a4458c80e4336b1fe2dc2c1e5066f
JavaTMP <support@javatmp.com>
2022-02-01 22:11:02 +0400

    add document processing module

---
+++ JavaTMP-Document-Processing/src/main/java/com/javatmp/document/main/JavaTmpDocumentProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.javatmp.document.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTmpDocumentProcessingApplication {
+
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        SpringApplication.run(JavaTmpDocumentProcessingApplication.class, args);
+    }
+
+}
---
+++ JavaTMP-Document-Processing/src/test/java/com/javatmp/document/main/JavaTmpDocumentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatmp.document.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTmpDocumentProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
